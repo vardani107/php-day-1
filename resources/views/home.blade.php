c4c844af7404659e75fb3e6c3c1ab58d4d8bd8b7
ilhmstxr <ilhambintanghuh30332@gmail.com>
2022-10-25 07:58:03 +0700

    edit home 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,20 @@
 @extends('master.master')
 
 @section('content')
-        {{-- <!--Hero-->
-        <section class="jumbotron text-center">
-            <img src="Puan_estetik.jpg?width=300&amp;height=300" alt="aaa" width="200" class=" rounded-circle img-thumbnail">
-            <h1 class="display-4" style="color: white">Puan Maharani-chan</h1>
-            <p class="lead" style="color:white">ketua Kader Bantenk Hitam</p>
-        </section>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#a10b0b" fill-opacity="1" d="M0,160L60,170.7C120,181,240,203,360,192C480,181,600,139,720,144C840,149,960,203,1080,218.7C1200,235,1320,213,1380,202.7L1440,192L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
-        <!--End of hero--> --}}
-<section class="jumbotron text-center"><h1>lmao</h1></section>
+    <!--Hero-->
+    <section class="jumbotron text-center">
+        <img src="Puan_estetik.jpg?width=300&amp;height=300" alt="aaa" width="200"
+            class=" rounded-circle img-thumbnail">
+        <h1 class="display-4" style="color: white">Puan Maharani-chan</h1>
+        <p class="lead" style="color:white">ketua Kader Bantenk Hitam</p>
+    </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#a10b0b" fill-opacity="1"
+            d="M0,160L60,170.7C120,181,240,203,360,192C480,181,600,139,720,144C840,149,960,203,1080,218.7C1200,235,1320,213,1380,202.7L1440,192L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z">
+        </path>
+    </svg>
+    <!--End of hero-->
+    <section class="jumbotron text-center">
+        <h1>lmao</h1>
+    </section>
 @endsection
